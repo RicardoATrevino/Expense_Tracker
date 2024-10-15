50a6cf03bf9b0f221eb4cf0f91f3ed15d77aa7ef
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-10-14 18:21:12 -0700

    added a few lines of code

--- main.py
+++ main.py
@@ -28,6 +28,20 @@ def initialize_csv():
   else:
     print(f'file already exists at {file_path}')
 
+# add expenses
+def add_expense():
+  date = datetime.now().strftime("%Y-%m-%d")
+  try:
+    amount = float(input("Enter Amount: "))
+    category = input("Enter a category: (e.g., food, transport)")
+    description = input("Enter a brief description")
+
+    with open(file_path, 'a', newline='') as file:
+      writer = csv.writer(file)
+      writer.writerow([date, amount, category, description])
+    print("Expense added successfully!")
+  except ValueError:
+    print("Invalid amount, please enter a valid number.")
 
 #View all expenses
 def view_expenses():
@@ -35,6 +49,79 @@ def view_expenses():
     print("No expenses recorded yet.")
     return
   
+  with open(file_path, 'r') as file:
+    reader = csv.reader(file)
+    next(reader) #this skips the header
+    for row in reader:
+      print(f"Date: {row[0]}, Amount: {row[1]}, Category: {row[2]}, Description: {row[3]}")
+
+  
+def delete_expense():
+  if not os.path.exists(file_path)
+    print("No expenses to delete")
+    return
+  
+  view_expenses()
+  try:
+    row_number = int(input("Enter Row Num to delete (starting from 1): "))
+    with open(file_path, 'r') as file :
+      lines = list(csv.reader(file))
+
+    if 1 < row_number <= len(lines):
+      del lines[row_number - 1]
+      with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(lines)
+      print("Expense deleted successfully!")
+    else:
+      print("Invalid row number.")
+  except ValueError:
+    print("No expenses recorded yet.")
+    return 
+
+
+# Function to view total expenses
+def view_total_expenses():
+    if not os.path.exists(file_path):
+        print("No expenses recorded yet.")
+        return
+
+    total = 0
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header
+        for row in reader:
+            total += float(row[1])
+    print(f"Total expenses: ${total:.2f}")
+
+
+def main():
+  initialize_csv()
+  while True:
+    print("\nExpense Tracker Menu:")
+    print("1. Add a new expense")
+    print("2. View all expenses")
+    print("3. Delete an expense")
+    print("4. View total expenses")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5):")
+    if choice == '1':
+      add_expense()
+    elif choice == '2':
+      view_expenses()
+    elif choice == '3':
+      delete_expense()
+    elif choice == '4':
+      view_total_expenses()
+    elif choice == '5':
+      print("Exiting the program.")
+      break
+    else:
+      print("Invalid choice. Please try again.")
+
+if __name__ == '__main__':
+  main()
 
 
 
